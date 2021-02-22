c472dd56207db39274b267de54da4088c21658c0
Documenter.jl <documenter@juliadocs.github.io>
2021-02-22 02:39:41 +0000

    build based on 7abe704

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Thermochron","category":"page"},{"location":"#Thermochron","page":"Home","title":"Thermochron","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Thermochron]","category":"page"}]
+}
