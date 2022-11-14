f713bb99eaec4415544fc951ee1b5b81b6ab39df
C. Brenhin Keller <cbkeller@dartmouth.edu>
2022-11-14 11:04:37 -0500

    Test modified `LinearAlgebra.lu!` to reuse tridiagonal `A.du2!` for pivoting

--- examples/ZrnHeInversionVartCryst.jl
+++ examples/ZrnHeInversionVartCryst.jl
@@ -34,9 +34,9 @@
 
 ## --- Prepare problem
 
-    burnin = 100000
+    burnin = 1000
     model = (
-        nsteps = 200000, # How many steps of the Markov chain should we run?
+        nsteps = 2000, # How many steps of the Markov chain should we run?
         burnin = burnin, # How long should we wait for MC to converge (become stationary)
         dr = 1.0,    # Radius step, in microns
         dt = 10.0,   # time step size in Myr
