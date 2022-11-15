6f2739d62927e3fcc3a2683fef79448d2e124901
C. Brenhin Keller <cbkeller@dartmouth.edu>
2022-11-14 19:10:16 -0500

    Update example manifest

--- examples/ZrnHeInversionVartCryst.jl
+++ examples/ZrnHeInversionVartCryst.jl
@@ -34,9 +34,9 @@
 
 ## --- Prepare problem
 
-    burnin = 1000
+    burnin = 350000
     model = (
-        nsteps = 2000, # How many steps of the Markov chain should we run?
+        nsteps = 500000, # How many steps of the Markov chain should we run?
         burnin = burnin, # How long should we wait for MC to converge (become stationary)
         dr = 1.0,    # Radius step, in microns
         dt = 10.0,   # time step size in Myr
