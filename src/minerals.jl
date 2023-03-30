f0ca249a5cb9d0bcc1782aba96f4d3723de3d1e2
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-03-29 23:27:26 -0400

    Add Apatite type

--- src/minerals.jl
+++ src/minerals.jl
@@ -108,7 +108,7 @@ function Zircon(r::T, dr::Number, Uppm::T, Thppm::T, dt::Number, agesteps::Abstr
     relVolumes = (rEdges[2:end].^3 - rEdges[1:end-1].^3)/rEdges[end]^3 # Relative volume fraction of spherical shell corresponding to each radius element
 
     # Alpha stopping distances for each isotope in each decay chain, from
-    # Farley et al., 1996
+    # Farley et al. (1996), doi: 10.1016/S0016-7037(96)00193-7
     alphaRadii238U = (11.78, 14.09, 13.73, 14.13, 17.32, 16.69, 28.56, 16.48,)
     alphaRadii235U = (12.58, 15.04, 19.36, 18.06, 23.07, 26.87, 22.47,)
     alphaRadii232Th = (10.99, 16.67, 18.16, 17.32, 23.61, 29.19,)
@@ -246,3 +246,185 @@ function Zircon(r::T, dr::Number, Uppm::T, Thppm::T, dt::Number, agesteps::Abstr
     )
 end
 export Zircon
+
+
+# Concretely-typed immutable struct to hold information about a single apatite (Helium) crystal
+struct Apatite{T<:Number} <: Mineral
+    dt::T
+    agesteps::Vector{T}
+    tsteps::Vector{T}
+    ntsteps::Int
+    dr::T
+    rsteps::Vector{T}
+    rEdges::Vector{T}
+    relVolumes::Vector{T}
+    nrsteps::Int
+    r238U::Vector{T}
+    r235U::Vector{T}
+    r232Th::Vector{T}
+    r238UHe::Vector{T}
+    r235UHe::Vector{T}
+    r232ThHe::Vector{T}
+    alphaDeposition::Matrix{T}
+    alphaDamage::Matrix{T}
+    annealedDamage::Matrix{T}
+    u::Matrix{T}
+    β::Vector{T}
+    Dcrystalline::Vector{T}
+    Damorphous::Vector{T}
+    A::Tridiagonal{T, Vector{T}}
+    ipiv::Vector{LinearAlgebra.BlasInt}
+    y::Vector{T}
+end
+# Constructor for the Apatite type, given grain radius, U and Th concentrations and t-T discretization information
+function Apatite(r::T, dr::Number, Uppm::T, Thppm::T, dt::Number, agesteps::AbstractVector{T}) where T<:Number
+
+    # Temporal discretization
+    tsteps = reverse(agesteps)
+    ntsteps = length(tsteps) # Number of time steps
+
+    # crystal size and spatial discretization
+    rsteps = Array{T}(0+dr/2 : dr: r-dr/2)
+    rEdges = Array{T}(0 : dr : r) # Edges of each radius element
+    nrsteps = length(rsteps)+2 # number of radial grid points -- note 2 implict points: one at negative radius, one outside grain
+    relVolumes = (rEdges[2:end].^3 - rEdges[1:end-1].^3)/rEdges[end]^3 # Relative volume fraction of spherical shell corresponding to each radius element
+
+    # Alpha stopping distances for each isotope in each decay chain, from
+    # Farley et al. (1996), doi: 10.1016/S0016-7037(96)00193-7
+    alphaRadii238U = (13.54, 16.26, 15.84, 16.31, 20.09, 22.89, 33.39, 19.10,)
+    alphaRadii235U = (14.48, 17.39, 22.5, 20.97, 26.89, 31.40, 26.18,)
+    alphaRadii232Th = (12.60, 19.32, 21.08, 20.09, 27.53, 34.14,)
+
+    # Jaffey decay constants
+    λ235U = log(2)/(7.0381*10^8)*10^6 # [1/Myr]
+    λ238U = log(2)/(4.4683*10^9)*10^6 # [1/Myr]
+    λ232Th = log(2)/(1.405*10^10)*10^6 # [1/Myr]
+
+    # Observed radial HPE profiles at present day
+    r238U = Uppm.*ones(T, size(rsteps)) # PPM
+    r235U = T.(r238U/137.818) #PPM
+    r232Th = Thppm .* ones(T, size(rsteps)) #PPM
+
+    # Convert to atoms per gram
+    r238U *= 6.022E23 / 1E6 / 238
+    r235U *= 6.022E23 / 1E6 / 235
+    r232Th *= 6.022E23 / 1E6 / 232
+
+    # Calculate effective He deposition for each decay chain, corrected for alpha
+    # stopping distance
+    dInt = Array{Float64}(undef, length(rEdges) - 1)
+
+    #238U
+    r238UHe = zeros(size(r238U))
+    @inbounds for ri = 1:length(rsteps)
+        for i=1:length(alphaRadii238U)
+            # Effective radial alpha deposition from 238U
+            intersectiondensity!(dInt,rEdges,relVolumes,alphaRadii238U[i],rsteps[ri])
+            @turbo @. r238UHe += relVolumes[ri] * dInt * r238U[ri]
+        end
+    end
+
+    #235U
+    r235UHe = zeros(size(r235U))
+    @inbounds for ri = 1:length(rsteps)
+        for i=1:length(alphaRadii235U)
+            # Effective radial alpha deposition from 235U
+            intersectiondensity!(dInt, rEdges,relVolumes,alphaRadii235U[i],rsteps[ri])
+            @turbo @. r235UHe += relVolumes[ri] * dInt * r235U[ri]
+        end
+    end
+
+    #232Th
+    r232ThHe = zeros(size(r232Th))
+    @inbounds for ri = 1:length(rsteps)
+        for i=1:length(alphaRadii232Th)
+            # Effective radial alpha deposition from 232Th
+            intersectiondensity!(dInt, rEdges,relVolumes,alphaRadii232Th[i],rsteps[ri])
+            @turbo @. r232ThHe += relVolumes[ri] * dInt * r232Th[ri]
+        end
+    end
+
+    # Alpha decay recoil damage
+    r238Udam = 8*r238U # No smoothing of alpha damage, 8 alphas per 238U
+    r235Udam = 7*r235U # No smoothing of alpha damage, 7 alphas per 235U
+    r232Thdam = 6*r232Th # No smoothing of alpha damage, 6 alphas per 232 Th
+
+    # Calculate corrected alpha deposition and recoil damage each time step for each radius
+    dt_2 = dt/2
+    decay = Array{T}(undef, ntsteps)
+    buffer = zeros(T, ntsteps, nrsteps-2)
+    # Allocate deposition and damage arrays
+    alphaDeposition = zeros(T, ntsteps, nrsteps-2)
+    alphaDamage = zeros(T, ntsteps, nrsteps-2)
+    # 238U
+    @turbo @. decay = exp(λ238U*(agesteps + dt_2)) - exp(λ238U*(agesteps - dt_2))
+    mul!(buffer, decay, r238UHe')
+    @turbo @. alphaDeposition += buffer
+    mul!(buffer, decay, r238Udam')
+    @turbo @. alphaDamage += buffer
+    # 235U
+    @turbo @. decay = exp(λ235U*(agesteps + dt_2)) - exp(λ235U*(agesteps - dt_2))
+    mul!(buffer, decay, r235UHe')
+    @turbo @. alphaDeposition += buffer
+    mul!(buffer, decay, r235Udam')
+    @turbo @. alphaDamage += buffer
+    # 232Th
+    @turbo @. decay = exp(λ232Th*(agesteps + dt_2)) - exp(λ232Th*(agesteps - dt_2))
+    mul!(buffer, decay, r232ThHe')
+    @turbo @. alphaDeposition += buffer
+    mul!(buffer, decay, r232Thdam')
+    @turbo @. alphaDamage += buffer
+
+    # Allocate additional variables that will be needed for Crank-Nicholson
+    annealedDamage = similar(alphaDamage)
+    β = Array{T}(undef, nrsteps) # First row of annealedDamage
+
+    # Allocate arrays for diffusivities
+    Dcrystalline = Array{T}(undef, ntsteps)
+    Damorphous = Array{T}(undef, ntsteps)
+
+    # Allocate output matrix for all timesteps
+    u = Array{T}(undef, nrsteps, ntsteps)
+
+    # Allocate variables for tridiagonal matrix and RHS
+    dl = ones(T, nrsteps-1)    # Sub-diagonal row
+    d = ones(T, nrsteps)       # Diagonal
+    du = ones(T, nrsteps-1)    # Supra-diagonal row
+    du2 = ones(T, nrsteps-2)   # sup-sup-diagonal row for pivoting
+    ipiv = Vector{LinearAlgebra.BlasInt}(undef, nrsteps) # For pivoting
+
+    # Tridiagonal matrix for LHS of Crank-Nicholson equation with regular grid cells
+    A = Tridiagonal(dl, d, du, du2)
+
+    # Vector for RHS of Crank-Nicholson equation with regular grid cells
+    y = Array{T}(undef, nrsteps)
+
+    return Apatite{T}(
+        dt,
+        agesteps,
+        tsteps,
+        ntsteps,
+        dr,
+        rsteps,
+        rEdges,
+        relVolumes,
+        nrsteps,
+        r238U,
+        r235U,
+        r232Th,
+        r238UHe,
+        r235UHe,
+        r232ThHe,
+        alphaDeposition,
+        alphaDamage,
+        annealedDamage,
+        u,
+        β,
+        Dcrystalline,
+        Damorphous,
+        A,
+        ipiv,
+        y,
+    )
+end
+export Apatite
