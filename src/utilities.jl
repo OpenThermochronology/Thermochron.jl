e8363d8126215f08466941ae8e6c501be2af8da1
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-11-03 17:45:30 -0500

    Disable custom accelerated `lu!`

--- src/utilities.jl
+++ src/utilities.jl
@@ -108,82 +108,82 @@
 
 ## --- Modify LinearAlgebra.lu! to reuse du2 for pivoting
 
-using LinearAlgebra: BlasInt, checknonsingular
-# Modified from LinearAlgebra stdlib to reuse `du2` and `ipiv`
-function lu!(A::Tridiagonal{T,V}, ipiv::Vector{BlasInt}, pivot::Union{RowMaximum,NoPivot} = RowMaximum(); check::Bool = true) where {T,V}
+# using LinearAlgebra: BlasInt, checknonsingular
+# # Modified from LinearAlgebra stdlib to reuse `du2` and `ipiv`
+# function lu!(A::Tridiagonal{T,V}, ipiv::Vector{BlasInt}, pivot::Union{RowMaximum,NoPivot} = RowMaximum(); check::Bool = true) where {T,V}
 
-    # Extract values
-    n = size(A, 1)
-    @assert length(ipiv) == n
+#     # Extract values
+#     n = size(A, 1)
+#     @assert length(ipiv) == n
 
-    # initialize variables
-    info = 0
-    dl = A.dl
-    d = A.d
-    du = A.du
-    if dl === du
-        throw(ArgumentError("off-diagonals of `A` must not alias"))
-    end
-    # Check if Tridiagonal matrix already has du2 for pivoting
-    d2len = max(0, n-2) # Proper length of a second off-diagonal
-    has_du2_defined = isdefined(A, :du2) && length(A.du2) == d2len
-    du2 = (has_du2_defined ? A.du2 : similar(d, d2len))::V
-    fill!(du2, 0)
+#     # initialize variables
+#     info = 0
+#     dl = A.dl
+#     d = A.d
+#     du = A.du
+#     if dl === du
+#         throw(ArgumentError("off-diagonals of `A` must not alias"))
+#     end
+#     # Check if Tridiagonal matrix already has du2 for pivoting
+#     d2len = max(0, n-2) # Proper length of a second off-diagonal
+#     has_du2_defined = isdefined(A, :du2) && length(A.du2) == d2len
+#     du2 = (has_du2_defined ? A.du2 : similar(d, d2len))::V
+#     fill!(du2, 0)
 
-    @inbounds begin
-        for i = 1:n
-            ipiv[i] = i
-        end
-        for i = 1:n-2
-            # pivot or not?
-            if pivot === NoPivot() || abs(d[i]) >= abs(dl[i])
-                # No interchange
-                if d[i] != 0
-                    fact = dl[i]/d[i]
-                    dl[i] = fact
-                    d[i+1] -= fact*du[i]
-                    du2[i] = 0
-                end
-            else
-                # Interchange
-                fact = d[i]/dl[i]
-                d[i] = dl[i]
-                dl[i] = fact
-                tmp = du[i]
-                du[i] = d[i+1]
-                d[i+1] = tmp - fact*d[i+1]
-                du2[i] = du[i+1]
-                du[i+1] = -fact*du[i+1]
-                ipiv[i] = i+1
-            end
-        end
-        if n > 1
-            i = n-1
-            if pivot === NoPivot() || abs(d[i]) >= abs(dl[i])
-                if d[i] != 0
-                    fact = dl[i]/d[i]
-                    dl[i] = fact
-                    d[i+1] -= fact*du[i]
-                end
-            else
-                fact = d[i]/dl[i]
-                d[i] = dl[i]
-                dl[i] = fact
-                tmp = du[i]
-                du[i] = d[i+1]
-                d[i+1] = tmp - fact*d[i+1]
-                ipiv[i] = i+1
-            end
-        end
-        # check for a zero on the diagonal of U
-        for i = 1:n
-            if d[i] == 0
-                info = i
-                break
-            end
-        end
-    end
-    B = has_du2_defined ? A : Tridiagonal{T,V}(dl, d, du, du2)
-    check && checknonsingular(info, pivot)
-    return LU{T,Tridiagonal{T,V},typeof(ipiv)}(B, ipiv, convert(BlasInt, info))
-end
+#     @inbounds begin
+#         for i = 1:n
+#             ipiv[i] = i
+#         end
+#         for i = 1:n-2
+#             # pivot or not?
+#             if pivot === NoPivot() || abs(d[i]) >= abs(dl[i])
+#                 # No interchange
+#                 if d[i] != 0
+#                     fact = dl[i]/d[i]
+#                     dl[i] = fact
+#                     d[i+1] -= fact*du[i]
+#                     du2[i] = 0
+#                 end
+#             else
+#                 # Interchange
+#                 fact = d[i]/dl[i]
+#                 d[i] = dl[i]
+#                 dl[i] = fact
+#                 tmp = du[i]
+#                 du[i] = d[i+1]
+#                 d[i+1] = tmp - fact*d[i+1]
+#                 du2[i] = du[i+1]
+#                 du[i+1] = -fact*du[i+1]
+#                 ipiv[i] = i+1
+#             end
+#         end
+#         if n > 1
+#             i = n-1
+#             if pivot === NoPivot() || abs(d[i]) >= abs(dl[i])
+#                 if d[i] != 0
+#                     fact = dl[i]/d[i]
+#                     dl[i] = fact
+#                     d[i+1] -= fact*du[i]
+#                 end
+#             else
+#                 fact = d[i]/dl[i]
+#                 d[i] = dl[i]
+#                 dl[i] = fact
+#                 tmp = du[i]
+#                 du[i] = d[i+1]
+#                 d[i+1] = tmp - fact*d[i+1]
+#                 ipiv[i] = i+1
+#             end
+#         end
+#         # check for a zero on the diagonal of U
+#         for i = 1:n
+#             if d[i] == 0
+#                 info = i
+#                 break
+#             end
+#         end
+#     end
+#     B = has_du2_defined ? A : Tridiagonal{T,V}(dl, d, du, du2)
+#     check && checknonsingular(info, pivot)
+#     return LU{T,Tridiagonal{T,V},typeof(ipiv)}(B, ipiv, convert(BlasInt, info))
+# end
