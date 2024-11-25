55edf937f229e1ed73ed5f717920ebadeb0e46a2
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-11-25 15:28:20 -0500

    Update Fanning Curvilinear models

--- src/fissiontrack.jl
+++ src/fissiontrack.jl
@@ -19,14 +19,10 @@ l₀ = 16.38 # [um] initial track length
 g(r, α ,β) = (((1-r^β)/β)^α-1)/α
 r(g, α, β) = (1-(g*α+1)^(1/α)*β)^(1/β)
 
-# From Ketcham, 1999
-DR1999 = FanningCurvilinear(-106.1806145, 2.196512263, -155.9001311, -9.786405925, -0.48078, -6.3626)
+# "Simultaneous fit" Fanning Curvilinear models 
+const FCKetcham1999 = FanningCurvilinear(-19.84402202, 0.3895104539,-51.25312954,-7.642358713,-0.12327,-11.988)
+const FCKetcham2007 = FanningCurvilinear(0.39528, 0.01073,-65.12969,-7.91715,0.04672,-1)
 
-# From Ketcham, 2007
-DR = FanningCurvilinear(-62.8742, 1.3060, -85.4861, -8.3589, -0.3900, -9.1435)
-RN = FanningCurvilinear(-41.2567, 0.8622, -65.8430, -7.9770, -0.3180, -3.1249)
-B3 = FanningCurvilinear(-19.0257, 0.4135, -25.8627, -7.1209, -0.2187, -19.3036)
-HS = FanningCurvilinear(-27.6610, 0.5880, -28.7268, -7.1267, -0.3720, -14.5778)
 
 function equivalenttime(t::Number, T::Number, Teq::Number, fc::FanningCurvilinear)
     exp(fc.C2 + (log(t*SEC_MYR)-fc.C2)*(log(1/(Teq+273.15))-fc.C3)/(log(1/(T+273.15))-fc.C3))/SEC_MYR
