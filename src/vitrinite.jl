5a1b0ce82dbbe2503863e3b878a6bd8aaf044281
Kalin T. McDannell <35042250+kmcdannell@users.noreply.github.com>
2026-08-31 21:19:44 -0400

    Update vitrinite.jl bug in Base.@kwdef and dt

--- src/vitrinite.jl
+++ src/vitrinite.jl
@@ -3,10 +3,18 @@
 # note that VR predictions for rapid thermal excursions are sensitive to agesteps density in that interval
 
 Base.@kwdef struct NielsenBasinRo{T<:AbstractFloat} <: VitriniteReflectanceModel{T}
-    logA::T = 60.9856   # ln(1/Ma), Nielsen et al. 2017 Table 3
-    Ea::Vector{T} = T[34.,36.,38.,40.,42.,44.,46.,48.,50.,52.,54.,56.,58.,60.,62.,64.,66.,68.,70.,72.] .* 4184.0  # J/mol
-    stoich::Vector{T} = normalize_stoich(T[0.0185,0.0143,0.0569,0.0478,0.0497,0.0344,0.0344,0.0322,0.0282,0.0062,0.1155,0.1041,0.1023,0.0760,0.0593,0.0512,0.0477,0.0086,0.0246,0.0096])
-    Ro0::T = 0.2104
+    logA::T
+    Ea::Vector{T}
+    stoich::Vector{T}
+    Ro0::T
+end
+function NielsenBasinRo(T::Type{<:AbstractFloat}=Float64;
+        logA::Number = 60.9856,
+        Ea::AbstractVector = [34.,36.,38.,40.,42.,44.,46.,48.,50.,52.,54.,56.,58.,60.,62.,64.,66.,68.,70.,72.] .* 4184.0,
+        stoich::AbstractVector = normalize_stoich([0.0185,0.0143,0.0569,0.0478,0.0497,0.0344,0.0344,0.0322,0.0282,0.0062,0.1155,0.1041,0.1023,0.0760,0.0593,0.0512,0.0477,0.0086,0.0246,0.0096]),
+        Ro0::Number = 0.2104,
+    )
+    NielsenBasinRo(T(logA), T.(Ea), T.(stoich), T(Ro0))
 end
 normalize_stoich(x) = x ./ sum(x)
 
@@ -29,7 +37,7 @@ function modelage(vro::VitriniteRo{T}, Tsteps::AbstractVector, model::NielsenBas
     xi = copy(model.stoich)
     @inbounds for i in eachindex(tsteps, Tsteps)
         Tavg = Tsteps[i] + ΔT
-        dt = step_at(tsteps, i) * SEC_MYR
+        dt = abs(step_at(tsteps, i)) * SEC_MYR   # dt is a duration -- must be positive
         for j in eachindex(xi, model.Ea)
             k = A * exp(-model.Ea[j] / (8.3143 * Tavg))
             xi[j] *= exp(-k*dt)
